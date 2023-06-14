817f69738e01439e205404ced7ff6183e6567c8e
WiZAKM007 <sitthachon46@gmail.com>
2023-06-14 15:08:33 +0700

    Winz

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
+//export default function App({ Component, pageProps }) {
+  //return <Component {...pageProps} />
+//}
 
 export default function App({
   Component,

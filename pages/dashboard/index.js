c40b799cf490913de4980227c9d1e8e5d601f7ec
WiZAKM007 <sitthachon46@gmail.com>
2023-06-14 14:24:01 +0700

    winz

---
+++ pages/dashboard/index.js
@@ -0,0 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as<br />
+         {session.user.fname} {session.user.lname}<br />
+        {session.user.email}<br />
+        {session.user.avatar}<br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
